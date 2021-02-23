5c71c996e49b28e030116f89672095c8bc42d96f
gknorman <gknorman@ARLWL0120011239.ndc.nasa.gov>
2021-02-22 17:45:39 -0800

    Troubleshooting los concurrency errors

---
+++ src/gov/nasa/cms/containers/GridPointPosition.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2021 United States Government as represented by the Administrator of the
+ * National Aeronautics and Space Administration.
+ * All Rights Reserved.
+ */
+
+package gov.nasa.cms.containers;
+
+import gov.nasa.worldwind.geom.Position;
+
+public class GridPoint
+{
+    private Position position;
+    private Position intersection;
+    private double relativeHeight;
+    private double trueElevation;
+
+}
