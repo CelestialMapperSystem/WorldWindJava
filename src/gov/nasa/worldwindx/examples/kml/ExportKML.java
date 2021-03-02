33247a18334162a2b31492eb470609e24eceb418
Kaitlyn Dickinson <kaitlynnd13@gmail.com>
2021-03-02 08:47:48 -0800

    Modified ExportKML to export document instead of writing to the screen

--- src/gov/nasa/worldwindx/examples/kml/ExportKML.java
+++ src/gov/nasa/worldwindx/examples/kml/ExportKML.java
@@ -44,73 +44,42 @@ public class ExportKML
 
     protected static Path makePath()
     {
-        Path path = new Path();
+        
 
         List<Position> positions = Arrays.asList(
             Position.fromDegrees(37.8304, -122.3720, 0),
-            Position.fromDegrees(37.8293, -122.3679, 50),
-            Position.fromDegrees(37.8282, -122.3710, 100));
+            Position.fromDegrees(37.8293, -122.3679, 0),
+            Position.fromDegrees(37.8282, -122.3710, 0));
+            Path path = new Path(positions);
 
-        path.setPositions(positions);
-        path.setExtrude(true);
 
-        path.setAttributes(normalShapeAttributes);
-        path.setHighlightAttributes(highlightShapeAttributes);
+       // path.setAttributes(normalShapeAttributes);
+       // path.setHighlightAttributes(highlightShapeAttributes);
 
-        path.setValue(AVKey.SHORT_DESCRIPTION, "Short description of Path");
-        path.setValue(AVKey.BALLOON_TEXT, "This is a Path.");
+     //   path.setValue(AVKey.SHORT_DESCRIPTION, "Short description of Path");
+     //   path.setValue(AVKey.BALLOON_TEXT, "This is a Path.");
 
         return path;
     }
 
     protected static Polygon makePolygon()
     {
-        Polygon poly = new Polygon();
+        
 
-        List<Position> outerBoundary = Arrays.asList(
-            Position.fromDegrees(37.8224479345424, -122.3739784354151, 50.0),
-            Position.fromDegrees(37.82239261906633, -122.3740285701554, 50.0),
-            Position.fromDegrees(37.82240608112512, -122.3744696934806, 50.0),
-            Position.fromDegrees(37.82228167878964, -122.3744693163394, 50.0),
-            Position.fromDegrees(37.82226619249474, -122.3739902862858, 50.0),
-            Position.fromDegrees(37.82219810227204, -122.3739510452131, 50.0),
-            Position.fromDegrees(37.82191990027978, -122.3742004406226, 50.0),
-            Position.fromDegrees(37.82186185177756, -122.3740740264531, 50.0),
-            Position.fromDegrees(37.82213350487949, -122.3738377669854, 50.0),
-            Position.fromDegrees(37.82213842777661, -122.3737599855226, 50.0),
-            Position.fromDegrees(37.82184815805735, -122.3735538230499, 50.0),
-            Position.fromDegrees(37.82188747252212, -122.3734202823307, 50.0),
-            Position.fromDegrees(37.82220302338508, -122.37362176179, 50.0),
-            Position.fromDegrees(37.8222686063349, -122.3735762207482, 50.0),
-            Position.fromDegrees(37.82224254303025, -122.3731468984375, 50.0),
-            Position.fromDegrees(37.82237319467147, -122.3731303943743, 50.0),
-            Position.fromDegrees(37.82238194814573, -122.3735637823936, 50.0),
-            Position.fromDegrees(37.82244505243797, -122.3736008458059, 50.0),
-            Position.fromDegrees(37.82274355652806, -122.3734009024945, 50.0),
-            Position.fromDegrees(37.82280084508153, -122.3735091430554, 50.0),
-            Position.fromDegrees(37.82251198652374, -122.3737489159765, 50.0),
-            Position.fromDegrees(37.82251207172572, -122.3738269699774, 50.0),
-            Position.fromDegrees(37.82280161524027, -122.3740332968739, 50.0),
-            Position.fromDegrees(37.82275318071796, -122.3741825267907, 50.0),
-            Position.fromDegrees(37.8224479345424, -122.3739784354151, 50.0));
+        List<Position> positions = Arrays.asList(
+            Position.fromDegrees(28, -106, 0),
+            Position.fromDegrees(35, -104, 0),
+            Position.fromDegrees(28, -107, 100),
+            Position.fromDegrees(28, -106, 0));
 
-        List<Position> innerBoundary = Arrays.asList(
-            Position.fromDegrees(37.82237624346899, -122.3739179072036, 50.0),
-            Position.fromDegrees(37.82226147323489, -122.3739053159649, 50.0),
-            Position.fromDegrees(37.82221834573171, -122.3737889140025, 50.0),
-            Position.fromDegrees(37.82226275093125, -122.3736772434448, 50.0),
-            Position.fromDegrees(37.82237889526623, -122.3736727730745, 50.0),
-            Position.fromDegrees(37.82243486851886, -122.3737811526564, 50.0),
-            Position.fromDegrees(37.82237624346899, -122.3739179072036, 50.0));
+       Polygon poly = new Polygon(positions);
+        
+      //  poly.setAltitudeMode(WorldWind.RELATIVE_TO_GROUND);
+        //poly.setAttributes(normalShapeAttributes);
+      //  poly.setHighlightAttributes(highlightShapeAttributes);
 
-        poly.setOuterBoundary(outerBoundary);
-        poly.addInnerBoundary(innerBoundary);
-        poly.setAltitudeMode(WorldWind.RELATIVE_TO_GROUND);
-        poly.setAttributes(normalShapeAttributes);
-        poly.setHighlightAttributes(highlightShapeAttributes);
-
-        poly.setValue(AVKey.SHORT_DESCRIPTION, "Short description of Polygon");
-        poly.setValue(AVKey.BALLOON_TEXT, "This is a Polygon.");
+        //poly.setValue(AVKey.SHORT_DESCRIPTION, "Short description of Polygon");
+        //poly.setValue(AVKey.BALLOON_TEXT, "This is a Polygon.");
 
         return poly;
     }
@@ -127,8 +96,10 @@ public class ExportKML
      */
     public static void main(String[] args)
     {
+        System.setProperty("gov.nasa.worldwind.app.config.document", "gov/nasa/cms/config/cmsConfiguration.xml");   
         try
         {
+           
             normalShapeAttributes = new BasicShapeAttributes();
             normalShapeAttributes.setInteriorMaterial(Material.BLUE);
             normalShapeAttributes.setOutlineMaterial(Material.BLACK);
@@ -138,11 +109,15 @@ public class ExportKML
             highlightShapeAttributes.setOutlineMaterial(Material.BLACK);
 
             // Create a StringWriter to collect KML in a string buffer
-            Writer stringWriter = new StringWriter();
+            //Writer stringWriter = new StringWriter();
 
+            // Create a new FileOutputStream to the user's home directory
+            OutputStream os = new FileOutputStream(Configuration.getUserHomeDirectory() + "/ExportKMLTest2.kml");
+            // Build the KML document from the file stream
+            KMLDocumentBuilder kmlBuilder = new KMLDocumentBuilder(os);
             // Create a document builder that will write KML to the StringWriter
-            KMLDocumentBuilder kmlBuilder = new KMLDocumentBuilder(stringWriter);
-
+//            KMLDocumentBuilder kmlBuilder = new KMLDocumentBuilder(stringWriter);
+//
             // Export the objects
             kmlBuilder.writeObjects(
                 makeSurfaceQuad(),
@@ -151,17 +126,17 @@ public class ExportKML
                 makePolygon());
 
             kmlBuilder.close();
-
-            // Get the exported document as a string
-            String xmlString = stringWriter.toString();
-
-            // Set up a transformer to pretty-print the XML
-            Transformer transformer = TransformerFactory.newInstance().newTransformer();
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-
-            // Write the pretty-printed document to stdout
-            transformer.transform(new StreamSource(new StringReader(xmlString)), new StreamResult(System.out));
+//
+//            // Get the exported document as a string
+//            String xmlString = stringWriter.toString();
+//
+//            // Set up a transformer to pretty-print the XML
+//            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+//            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+//            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+//
+//            // Write the pretty-printed document to stdout
+//            transformer.transform(new StreamSource(new StringReader(xmlString)), new StreamResult(System.out));
         }
         catch (Exception e)
         {
