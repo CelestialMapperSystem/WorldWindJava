d6b855ffdee3c1750267b57c75aa50f2260fbc74
Kaitlyn Dickinson <kaitlynnd13@gmail.com>
2021-03-25 10:15:58 -0700

    Skeleton for data-import complete, fixing null pointer issues

--- src/gov/nasa/cms/features/ImportedDataPanel.java
+++ src/gov/nasa/cms/features/ImportedDataPanel.java
@@ -6,16 +6,37 @@
 package gov.nasa.cms.features;
 
 import gov.nasa.cms.CelestialMapper;
+import gov.nasa.cms.LayerPanel;
+import gov.nasa.cms.features.layermanager.LayerManagerDialog;
 import gov.nasa.cms.util.PanelTitle;
 import gov.nasa.cms.util.ShadedPanel;
+import gov.nasa.worldwind.Factory;
+import gov.nasa.worldwind.WorldWind;
 import gov.nasa.worldwind.WorldWindow;
+import gov.nasa.worldwind.avlist.AVKey;
+import gov.nasa.worldwind.avlist.AVList;
+import gov.nasa.worldwind.geom.Angle;
+import gov.nasa.worldwind.geom.Extent;
+import gov.nasa.worldwind.geom.Position;
+import gov.nasa.worldwind.geom.Sector;
+import gov.nasa.worldwind.globes.ElevationModel;
+import gov.nasa.worldwind.layers.Layer;
+import gov.nasa.worldwind.terrain.CompoundElevationModel;
 import java.awt.BorderLayout;
 import javax.swing.JPanel;
 import gov.nasa.worldwind.util.*;
+import gov.nasa.worldwindx.applications.worldwindow.core.layermanager.LayerPath;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
 import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+import org.w3c.dom.Element;
 
 /**
  *
@@ -23,10 +44,14 @@ import javax.swing.SwingConstants;
  */
 public class ImportedDataPanel extends ShadedPanel
 {
+
     private WorldWindow wwd;
     private CelestialMapper cms;
     protected JPanel dataConfigPanel;
-    
+    private JButton importButton;
+    private JButton goToButton;
+    private JPanel buttonPanel ;
+
     public ImportedDataPanel(WorldWindow wwd)
     {
         super(new BorderLayout());
@@ -41,7 +66,7 @@ public class ImportedDataPanel extends ShadedPanel
         this.wwd = wwd;
         this.makePanel();
     }
-    
+
     protected void makePanel()
     {
         this.add(new PanelTitle("Imported Data", SwingConstants.CENTER), BorderLayout.NORTH);
@@ -67,7 +92,195 @@ public class ImportedDataPanel extends ShadedPanel
         bodyPanel.setBorder(BorderFactory.createEmptyBorder(9, 9, 9, 9));
         bodyPanel.add(scrollPane, BorderLayout.CENTER);
 
+        buttonPanel = new JPanel(new BorderLayout());
+        buttonPanel.setOpaque(false);
+
+        this.add(buttonPanel, BorderLayout.SOUTH);
         this.add(bodyPanel, BorderLayout.CENTER);
     }
+
+    /**
+     * Adds the UI components for the specified imported data to this panel, and
+     * adds the WorldWind component created from the data to the WorldWindow
+     * passed to this panel during construction.
+     *
+     * @param domElement the document which describes a WorldWind data
+     * configuration.
+     * @param params the parameter list which overrides or extends information
+     * contained in the document.
+     *
+     * @throws IllegalArgumentException if the Element is null.
+     */
+    public void addImportedData(final Element domElement, final AVList params)
+    {
+        if (domElement == null)
+        {
+            String message = Logging.getMessage("nullValue.DocumentIsNull");
+            Logging.logger().severe(message);
+            throw new IllegalArgumentException(message);
+        }
+
+        this.addToWorldWindow(domElement, params);
+
+        String description = this.getDescription(domElement);
+        Sector sector = this.getSector(domElement);
+
+        Box box = Box.createHorizontalBox();
+        box.setOpaque(false);
+        box.add(new JButton(new GoToSectorAction(sector)));
+        box.add(Box.createHorizontalStrut(10));
+        JLabel descLabel = new JLabel(description);
+        descLabel.setOpaque(false);
+        box.add(descLabel);
+
+        this.dataConfigPanel.add(box);
+        this.revalidate();
+    }
+
+    protected String getDescription(Element domElement)
+    {
+        String displayName = DataConfigurationUtils.getDataConfigDisplayName(domElement);
+        String type = DataConfigurationUtils.getDataConfigType(domElement);
+
+        StringBuilder sb = new StringBuilder(displayName);
+
+        if (type.equalsIgnoreCase("Layer"))
+        {
+            sb.append(" (Layer)");
+        } else if (type.equalsIgnoreCase("ElevationModel"))
+        {
+            sb.append(" (Elevations)");
+        }
+
+        return sb.toString();
+    }
+
+    protected Sector getSector(Element domElement)
+    {
+        return WWXML.getSector(domElement, "Sector", null);
+    }
+
+    protected void addToWorldWindow(Element domElement, AVList params)
+    {
+        String type = DataConfigurationUtils.getDataConfigType(domElement);
+        if (type == null)
+        {
+            return;
+        }
+
+        if (type.equalsIgnoreCase("Layer"))
+        {
+            this.addLayerToWorldWindow(domElement, params);
+        } else if (type.equalsIgnoreCase("ElevationModel"))
+        {
+            this.addElevationModelToWorldWindow(domElement, params);
+        }
+    }
+
+    protected void addLayerToWorldWindow(Element domElement, AVList params)
+    {
+        try
+        {
+            Factory factory = (Factory) WorldWind.createConfigurationComponent(AVKey.LAYER_FACTORY);
+            Layer layer = (Layer) factory.createFromConfigSource(domElement, params);
+            if (layer != null)
+            {
+                layer.setEnabled(true);
+                this.addLayer(layer, new LayerPath("Imported"));
+            }
+        } catch (Exception e)
+        {
+            String message = Logging.getMessage("generic.CreationFromConfigurationFailed",
+                    DataConfigurationUtils.getDataConfigDisplayName(domElement));
+            Logging.logger().log(java.util.logging.Level.SEVERE, message, e);
+        }
+    }
+
+    protected void addLayer(final Layer layer, final LayerPath pathToParent)
+    {
+        SwingUtilities.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+                LayerPath path = new LayerPath(pathToParent, layer.getName());
+                doAddLayer(layer, path);
+            }
+        });
+    }
+
+    protected void doAddLayer(final Layer layer, final LayerPath path)
+    {
+        wwd.getModel().getLayers().add(layer);
+    }
+
+    protected void addElevationModelToWorldWindow(Element domElement, AVList params)
+    {
+        ElevationModel em = null;
+        try
+        {
+            Factory factory = (Factory) WorldWind.createConfigurationComponent(AVKey.ELEVATION_MODEL_FACTORY);
+            em = (ElevationModel) factory.createFromConfigSource(domElement, params);
+        } catch (Exception e)
+        {
+            String message = Logging.getMessage("generic.CreationFromConfigurationFailed",
+                    DataConfigurationUtils.getDataConfigDisplayName(domElement));
+            Logging.logger().log(java.util.logging.Level.SEVERE, message, e);
+        }
+
+        if (em == null)
+        {
+            return;
+        }
+
+        ElevationModel defaultElevationModel = wwd.getModel().getGlobe().getElevationModel();
+        if (defaultElevationModel instanceof CompoundElevationModel)
+        {
+            if (!((CompoundElevationModel) defaultElevationModel).containsElevationModel(em))
+            {
+                ((CompoundElevationModel) defaultElevationModel).addElevationModel(em);
+            }
+        } else
+        {
+            CompoundElevationModel cm = new CompoundElevationModel();
+            cm.addElevationModel(defaultElevationModel);
+            cm.addElevationModel(em);
+            wwd.getModel().getGlobe().setElevationModel(cm);
+        }
+    }
+
+    protected class GoToSectorAction extends AbstractAction
+    {
+
+        protected Sector sector;
+
+        public GoToSectorAction(Sector sector)
+        {
+            super("Go To");
+            this.sector = sector;
+            this.setEnabled(this.sector != null);
+        }
+
+        public void actionPerformed(ActionEvent e)
+        {
+            Extent extent = Sector.computeBoundingCylinder(wwd.getModel().getGlobe(),
+                    wwd.getSceneController().getVerticalExaggeration(), this.sector);
+
+            Angle fov = wwd.getView().getFieldOfView();
+            Position centerPos = new Position(this.sector.getCentroid(), 0d);
+            double zoom = extent.getRadius() / fov.cosHalfAngle() / fov.tanHalfAngle();
+
+            wwd.getView().goTo(centerPos, zoom);
+        }
+    }
+
+    public JPanel getButtonPanel()
+    {
+        return buttonPanel;
+    }
     
+    public JButton getImportButton()
+    {
+        return importButton;
+    }
+
 }
