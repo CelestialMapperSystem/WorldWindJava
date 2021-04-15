70a714f2aa19c7fd3e892a2cd50099f183a587de
gknorman <gknorman@ARLWL0120011239.ndc.nasa.gov>
2021-04-15 16:59:47 -0700

    Completed merge and commented out my system path to cacerts

--- src/gov/nasa/cms/CelestialMappingSystem.java
+++ src/gov/nasa/cms/CelestialMappingSystem.java
@@ -15,16 +15,11 @@ public class CelestialMappingSystem
     public static void main(String[] args) 
     {  
         // Set the WorldWind Configuration document to be overriden by CMS properties
-        System.setProperty("gov.nasa.worldwind.app.config.document", "gov/nasa/cms/config/cmsConfiguration.xml");
-        Path path = Paths.get("C:\\Users\\gknorman\\cms-main\\WorldWindJava"
-            + "-cms\\security\\cacerts");
-        System.setProperty ("javax.net.ssl.trustStore", String.valueOf(path));
-        System.setProperty ("javax.net.ssl.trustStorePassword", "changeit");
-
-
         if (Configuration.isMacOS()) {
             System.setProperty("com.apple.mrj.application.apple.menu.about.name", APP_NAME);
         }
+
+        
 //        Path path = Paths.get("C:\\Users\\gknorman\\cms-main\\WorldWindJava-cms"
 //            + "\\security\\cacerts"); // Or wherever you created your local cacerts file
 //        System.setProperty ("javax.net.ssl.trustStore", String.valueOf(path));
